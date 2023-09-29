b2efb9ce17309d14cc662f1d86baf8f570046cff
luissanchezb <94795076+luissanchezb@users.noreply.github.com>
2023-09-29 17:26:01 -0500

    Practica3Resuelta

---
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -0,0 +1,19 @@
+
+package com.example.marsphotos.data
+
+import com.example.marsphotos.model.MarsPhoto
+import com.example.marsphotos.network.MarsApiService
+
+
+interface MarsPhotosRepository {
+    /** Fetches list of MarsPhoto from marsApi */
+    suspend fun getMarsPhotos(): List<MarsPhoto>
+}
+
+
+class NetworkMarsPhotosRepository(
+    private val marsApiService: MarsApiService
+) : MarsPhotosRepository {
+    /** Fetches list of MarsPhoto from marsApi*/
+    override suspend fun getMarsPhotos(): List<MarsPhoto> = marsApiService.getPhotos()
+}
